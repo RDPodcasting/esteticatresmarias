4d2895b9fb8978031536d396e13e6304f98b7156
rdpodcasting <remediosdemaispodcast@gmail.com>
2020-08-24 21:10:12 -0300

    jsonp send

--- js/compartilhar-whatsapp.js
+++ js/compartilhar-whatsapp.js
@@ -1,6 +1,6 @@
 function whatsApp(Nome,Numero,Dia,Servico)
 {
-    var url;
+    var endereco;
     var texto="Sou #NOME#, meu número é o #NUMERO# e, quero agendar um horário no dia #DIA# para fazer #SERVICO#, vim do site esteticatresmarias.com.br e aguardo uma confirmação com valor e formas de pagamento que vocês aceitam!";
     texto=texto.replace('#NOME#',Nome);
     texto=texto.replace('#NUMERO#',Numero);
@@ -11,20 +11,25 @@ function whatsApp(Nome,Numero,Dia,Servico)
     var telefone="5511949389623";
     if($.isMobile)
     {
-        url="whatsapp://send?phone="+telefone+"&text="+texto
+        endereco="whatsapp://send?phone="+telefone+"&text="+texto
     }
     else
     {
-        url="https://api.whatsapp.com/send?phone="+telefone+"&text="+texto
+        endereco="https://api.whatsapp.com/send?phone="+telefone+"&text="+texto
     }
-    console.log("Enviando msg: "+texto);
-    $.post( url, $('form#myForm').serialize(), function(data) {
-         // ... do something with response from server
-         console.log(data);
-
-       },
-       'json' // I expect a JSON response
-    );
+    console.log("Enviando msg para: "+endereco);
+    $.ajax({
+        type : "POST",
+        dataType : "jsonp",
+        url : endereco,
+        success: function(data){
+            alert("Menssagem enviada com sucesso!");
+        },
+        error: function(ex){
+            alert("Não foi possível enviar a menssagem, tente novamente mais tarde!");
+        }
+   });
+   
 }
 
 $('#agendar').click( function() {
